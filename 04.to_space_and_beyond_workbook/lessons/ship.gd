4c0e8a2d41e520d14cd5a05bc90cbb52b4828790
707bremo <baremo@s.sfusd.edu>
2024-03-15 12:27:01 -0700

    L4 The Boosting Ship
    A practice set, giving us code to allow the ship to boost when pressing a key.

--- 04.to_space_and_beyond_workbook/lessons/ship.gd
+++ 04.to_space_and_beyond_workbook/lessons/ship.gd
@@ -2,6 +2,8 @@ extends Sprite2D
 
 var max_speed := 600.0
 var velocity := Vector2(0,0)
+var boost_speed := 1500.0
+var normal_speed := 600.0
 
 func _process(delta: float) -> void:
 	var direction := Vector2(0,0)
@@ -14,5 +16,15 @@ func _process(delta: float) -> void:
 
 	velocity = direction * max_speed
 	position += velocity * delta
+
 	if direction.length() > 0.0:
 		rotation = velocity.angle()
+
+	if Input.is_action_just_pressed("boost"):
+		max_speed = boost_speed
+		get_node("Timer").start()
+		
+
+
+func _on_timer_timeout() -> void:
+	max_speed = normal_speed
